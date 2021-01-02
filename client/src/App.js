a2d536a5821d897e1817c3bef3134d886cc91f63
Andrew Crow <andrew@crow.me>
2021-01-01 21:26:53 -0800

    changed everything over for mern stack

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-
-  const [apiResponse, setApiResponse] = useState("")
-
-  const callAPI = () => {
-    fetch("http://localhost:9000/testAPI")
-      .then(res => res.text())
-      .then(res => setApiResponse({ response: res }));
-  }
-
-  useEffect(() => {
-    callAPI()
-  }, [])
-
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {apiResponse.response}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
     </div>
   );
 }
+
 
 export default App;
